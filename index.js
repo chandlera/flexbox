d688deb644b5741a0ffea1a85286644993bf8643
Alan Chandler <chandlera718@gmail.com>
2015-08-14 14:38:24 -0700

    Set up an initial base for flexbox tutorial

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = parseInt(process.env.PORT, 10) || 4567;
+var publicDir = process.argv[2] || __dirname + '/public';
+
+app.get("/", function(req, res) {
+  res.redirect('/index.html');
+});
+
+app.use(express.static(publicDir));
+
+console.log('Simple static server showing %s listening at http://%s:%s', publicDir, hostname, port);
+app.listen(port, hostname);
