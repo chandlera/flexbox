bce6739cc69fe8303d58624193fe955124bb265a
Alan Chandler <alan.chandler@intrepidlearning.com>
2015-08-19 16:43:31 -0700

    Video 14: Flexbox navigation code-along
    Also made some changes to gulpfile to rerun node as well when
    making changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var nodemon = require('gulp-nodemon');
+var jshint = require('gulp-jshint');
 
 gulp.task('styles', function () {
     gulp.src('public/style.css')
         .pipe(autoprefixer())
         .pipe(gulp.dest('public/build'));
+});
+
+gulp.task('lint', function () {
+  gulp.src('./**/*.js')
+    .pipe(jshint());
+});
+
+gulp.task('develop', function () {
+  nodemon({ script: 'index.js',
+            ext: 'html js css',
+            ignore: ['ignored.js'],
+            tasks: ['styles']
+          })
+    .on('restart', function () {
+      console.log('restarted!');
+    });
 });
 
 gulp.task('watch', function() {
